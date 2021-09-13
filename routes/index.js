1ec2a6af0dc4297deba91506933cf2a4fa631c73
Hhtch <xwisefoxx@yandex.ua>
2021-09-13 18:34:47 +0300

    Добавил чтение txt и их файл стат.

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ const { readdir } = require('fs/promises');
 const { readFile } = require('fs/promises');
 //const { stat } = require ('fs/promises');
 const path = require('path');
-
-
+const open = require('fs/promises');
+const close = require('fs/promises');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -23,12 +23,14 @@ router.get('/', function (req, res, next) {
         const stat = await fs.stat(newPath);
         if (stat.isFile()) {
           console.log("'%s' is a file.", newPath);
-          if(newPath == `*.txt`){
-            console.log("'%s' open file.", newPath);
+          if (path.extname(file) == ".txt") {
+            console.log("Open file '%s'.", file);
+            read(newPath);
+           
           };
-            // нужно сделать проверку на тхт и если это он открыть если нет дальше перебирать
+          // нужно сделать проверку на тхт и если это он открыть если нет дальше перебирать
         } else if (stat.isDirectory()) {
-            console.log("'%s' is a directory.", newPath);
+          console.log("'%s' is a directory.", newPath);
         }
       }
       //await read(file);
@@ -37,22 +39,39 @@ router.get('/', function (req, res, next) {
     }
 
   }
-
+    
 
   async function read(fileName) {
-    try {
-      const controller = new AbortController();
-      const { signal } = controller;
-      const promise = readFile(fileName, { signal });
+    let file;
+try {
+  file = await fs.open(fileName, 'r');
+  const stat = await file.stat();
+  // use stat
+    console.log(stat);
+    const promise = readFile(fileName,'utf8', 'r' );
+    await promise;
+    console.log(promise);
+} catch (err) {
+  // When a request is aborted - err is an AbortError
+  console.error(err);
+} finally {
+  await file.close(fileName);
+}
 
+
+    /*try {
+      //const controller = new AbortController();
+      //const { signal } = controller;
+      //const promise = readFile(fileName, { signal });
+      const promise = readFile(fileName,'utf8', 'r' );
       // Abort the request before the promise settles.
-      controller.abort();
+      //controller.abort();
 
       await promise;
     } catch (err) {
       // When a request is aborted - err is an AbortError
       console.error(err);
-    }
+    }*/
   }
   momo();
   //read(`D:\\1\\123.txt`);
