db15485b71c103ae7cbc47949078ea43fe967c19
Hhtch <xwisefoxx@yandex.ua>
2021-09-15 18:33:44 +0300

    открываю тхт и другие файлы

--- public/javascripts/dir.js
+++ public/javascripts/dir.js
@@ -1,66 +1,41 @@
-async function getFileOrDir()    {
+async function getFileOrDir() {
   let response = await fetch('/dir', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(),
   });
- 
+
   result = await response.json();
   let Files = result.File;
   let Dirs = result.Directory;
-/*  
-  let p = document.createElement("p");
-  document.body.appendChild(p);
 
-  
-
-
-
-// родительский элемент UL
-02
-var list = document.getElementById('list2')
-03
-// элемент для вставки перед ним (первый LI)
-04
-var firstLi = list.getElementsByTagName('LI')[0]
-05
- 
-06
-// новый элемент
-07
-var newListElem = document.createElement('LI')
-08
-newListElem.innerHTML = 'Новый элемент списка'
-09
- 
-10
-// вставка
-11
-list.insertBefore(newListElem, firstLi)
-*/
-
-
-
-  for  ( let i=0; i < Files.length; i++){
-     //console.log(Files[i]);
+  for (let i = 0; i < Files.length; i++) {    
     let list = document.getElementById('files');
-    let li = document.createElement('Li');
-    li.innerHTML = Files[i];
-    list.appendChild(li);
-    
+    let div = document.createElement('div');
+    div.setAttribute('class', 'flex-item');
+    div.addEventListener ("click", () => {getTxt( Files[i] )}, false);
+    let p = document.createElement('p');
+    var img = document.createElement("img");
+    img.src = "/images/text_file.png";
+    list.appendChild(div);
+    p.appendChild(document.createTextNode(Files[i]));
+    div.appendChild(img);
+    div.appendChild(p);
   };
 
-  for  ( let i=0; i < Dirs.length; i++){
-    //console.log(Files[i]);
-   let list = document.getElementById('dirs'); 
-   let li = document.createElement('li');
-   li.innerHTML = Dirs[i];
-   list.appendChild(li);
- };
-  //console.log(Dirs);
-  //console.log(Files);
-    
+  for (let i = 0; i < Dirs.length; i++) {
+    let list = document.getElementById('dirs');
+    let div = document.createElement('div');
+    div.setAttribute('class', 'flex-item');
+    let p = document.createElement('p');
+    var img = document.createElement("img");
+    img.src = "/images/folder.png";
+    list.appendChild(div);
+    p.appendChild(document.createTextNode(Dirs[i]));
+    div.appendChild(img);
+    div.appendChild(p);
+  }
 }
 getFileOrDir();
--- public/javascripts/txt.js
+++ public/javascripts/txt.js
@@ -1,4 +1,4 @@
-async function getTxt()    {
+async function getTxt( fileName )    {
     let response = await fetch('/txt', {
         method: 'POST',
         headers: {
@@ -6,8 +6,20 @@ async function getTxt()    {
         },
         body: JSON.stringify(),
     });
-
+    let listName = [];
+    let listText = [];
     result = await response.json();
-    console.log(result);        
+    for (let i = 0; i < result.length; i++){
+      listName.push( result[i].Name );
+      listText.push( result[i].Text );      
+    }
+   // console.log(listName);
+   //console.log(TelistTextxt);
+    for (let i = 0; i < listName.length; i++) {
+      Name = listName[i];      
+      if ( Name == fileName){
+        console.log(Name);
+      }
+    }
+    if(listName.indexOf( fileName ) == -1 ) console.log( `Не умею открывать этот файл ${fileName}` ) 
 }
-getTxt();
